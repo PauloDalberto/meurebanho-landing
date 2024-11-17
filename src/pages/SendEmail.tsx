cb1c4654296547975478cb48b4bae10df8af0ccc
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2024-11-17 15:09:37 -0300

    refac: add toast on page

--- src/pages/SendEmail.tsx
+++ src/pages/SendEmail.tsx
@@ -1,19 +1,24 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
+import { ToastSuccess } from '../components/ToastSuccess';
+import { ToastError } from '../components/ToastError';
 import '../styles/sendEmail.css';
+
 
 export default function SendEmail() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [toastSuccessOpen, setToastSuccessOpen] = useState(false); 
+  const [toastErrorOpen, setToastErrorOpen] = useState(false); 
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  
+
     const body = JSON.stringify({
       toMail: email,
       content: message,
     });
-  
+
     try {
       const response = await fetch('/api', {
         method: 'POST',
@@ -23,16 +28,19 @@ export default function SendEmail() {
         },
         body: body,
       });
-      
+
       if (!response.ok) {
         const errorText = await response.text();
         console.error('Erro ao enviar e-mail:', errorText);
+        setToastErrorOpen(true); 
       } else {
-        console.log('E-mail enviado com sucesso!');
+        setToastSuccessOpen(true); 
+        setEmail(''); 
+        setMessage(''); 
       }
-  
     } catch (error) {
       console.error('Erro na requisição:', error);
+      setToastErrorOpen(true); 
     }
   };
 
@@ -41,7 +49,9 @@ export default function SendEmail() {
       <div className='sendEmail-header'>
         <span>Envie sua dúvida</span>
         <h1>Entre em contato</h1>
-        <p>Entre em contato, estamos dispostos a tirar qualquer dúvida, seja um orçamento, uma dúvida técnica de algum de nossos produtos. Estamos à disposição para responder.</p>
+        <p>
+          Entre em contato, estamos dispostos a tirar qualquer dúvida, seja um orçamento, uma dúvida técnica de algum de nossos produtos. Estamos à disposição para responder.
+        </p>
       </div>
 
       <form className='inputs' onSubmit={handleSubmit}>
@@ -59,6 +69,9 @@ export default function SendEmail() {
         />
         <Button text='Enviar' type="submit" />
       </form>
+
+      <ToastSuccess open={toastSuccessOpen} setOpen={setToastSuccessOpen} />
+      <ToastError open={toastErrorOpen} setOpen={setToastErrorOpen} />
     </div>
   );
 }
